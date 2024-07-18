b82ba029583a0f6335efd7a03395014b300e70aa
James Kemboi <jameskemich@gmail.com>
2024-07-19 01:24:53 +0300

    project-continue-3

--- main.js
+++ main.js
@@ -31,3 +31,20 @@ ScrollReveal().reveal(".header__container .btn",{
   ...scrollRevealOption,
   delay:1000,
 });
+// room container
+ScrollReveal().reveal(".room__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// feature container
+ScrollReveal().reveal(".feature__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// news container
+ScrollReveal().reveal(".news__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
